6241f9d9a9ac85fd66a6c3b7b6a0063413daf2b9
matt-stutzman <112518765+matt-stutzman@users.noreply.github.com>
2025-02-10 21:14:44 -0700

    Config for playwright testing
    I should get this done within the next couple of days

---
+++ tests/pizza.spec.js
@@ -0,0 +1,12 @@
+import { test, expect } from 'playwright-test-coverage';
+
+test('home page', async ({ page }) => {
+  await page.goto('/');
+
+  expect(await page.title()).toBe('JWT Pizza');
+});
+
+test('buy pizza with login', async ({ page }) => {
+
+    
+});
